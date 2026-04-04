b4f4b1a429cbf8acb9e38089f0e284aa150af1ff
ChravelApp <christian@chravelapp.com>
2026-04-03 23:16:55 -0700

    Triage root technical debt and remove stale artifacts

--- fix-conflict-1.sh
+++
@@ -1,3 +0,0 @@
-sed -i '/<<<<<<< HEAD/d' src/components/AIConciergeChat.tsx
-sed -i '/=======/d' src/components/AIConciergeChat.tsx
-sed -i '/>>>>>>> origin\/main/d' src/components/AIConciergeChat.tsx
